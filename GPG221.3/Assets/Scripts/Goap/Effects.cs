6b2aaba8a0b8aef931eda917af951424af40c85f
Efim Egorov <efim.egorov@student.saedubai.com>
2025-08-16 21:40:56 +0400

    Reapply "fixed error related"
    This reverts commit 6b706831bc447d5131feefbdfa95d8abe6dd6181.

--- GPG221.3/Assets/Scripts/Goap/Effects.cs
+++ GPG221.3/Assets/Scripts/Goap/Effects.cs
@@ -1,8 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class Effect 
+namespace Goap
 {
-    public string name;
+    [System.Serializable]
+    public class Effect 
+    {
+        public string name;
+    }
 }
